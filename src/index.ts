e0b0828ef70041e6ad9b17915163ed103530d18a
Ruben Straube <github@r10e.de>
2021-12-01 06:52:32 +0100

    2*

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,19 @@ const input: number[] = readFileSync('src/input.txt', 'utf8')
 	.split('\n')
 	.map((x: string) => parseInt(x));
 
-
-let increases = 0;
+let threes: number[] = [];
 
 input.forEach((depth, index) => {
-	if (index > 0 && depth > input[index - 1]) {
+	if (index >= 2) {
+		const lastThree = depth + input[index - 1] + input[index - 2];
+		threes.push(lastThree);
+	}
+});
+let increases = 0;
+
+threes.forEach((sumOfThree, index) => {
+	const lastSum = threes[index - 1];
+	if (index > 0 && sumOfThree > lastSum) {
 		increases++;
 	}
 });
