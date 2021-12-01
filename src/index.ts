2e88d709c83676e17c7679e80660a0e458ae5784
Ruben Straube <github@r10e.de>
2021-12-01 06:40:59 +0100

    1*

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-// happy coding 👻
-console.log("hello world");
+import { readFileSync } from 'fs';
+
+const input: number[] = readFileSync('src/input.txt', 'utf8')
+	.split('\n')
+	.map((x: string) => parseInt(x));
+
+
+let increases = 0;
+
+input.forEach((depth, index) => {
+	if (index > 0 && depth > input[index - 1]) {
+		increases++;
+	}
+});
+
+console.log(increases);
