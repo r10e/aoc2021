bf8cb071c4081702a113a95047a1e3be528064b4
Ruben Straube <github@r10e.de>
2021-12-02 07:17:12 +0100

    day2

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,87 @@
 import { readFileSync } from 'fs';
 
-const input: number[] = readFileSync('src/input.txt', 'utf8')
-	.split('\n')
-	.map((x: string) => parseInt(x));
-
-let threes: number[] = [];
-
-input.forEach((depth, index) => {
-	if (index >= 2) {
-		const lastThree = depth + input[index - 1] + input[index - 2];
-		threes.push(lastThree);
+class Instruction {
+	constructor(input: string = '') {
+		const [direction, by] = input.split(' ');
+		switch (direction) {
+			case 'forward': {
+				this.forward = parseInt(by);
+				break;
+			}
+			case 'down': {
+				this.down = parseInt(by);
+				break;
+			}
+			case 'up': {
+				this.up = parseInt(by);
+				break;
+			}
+		}
 	}
-});
-let increases = 0;
+	forward: number = 0;
+	down: number = 0;
+	up: number = 0;
+}
 
-threes.forEach((sumOfThree, index) => {
-	const lastSum = threes[index - 1];
-	if (index > 0 && sumOfThree > lastSum) {
-		increases++;
+class Position {
+	constructor(horizontal: number = 0, depth: number = 0, aim: number = 0) {
+		this.horizontal = horizontal;
+		this.depth = depth;
+		this.aim = aim;
 	}
-});
+	horizontal: number;
+	depth: number;
+	aim: number;
 
-console.log(increases);
+
+	move1(by: Instruction): Position {
+		return new Position(
+			this.horizontal + by.forward,
+			this.depth + by.down - by.up,
+			this.aim
+		);
+	}
+
+	move2(by: Instruction): Position {
+		if (by.forward) {
+			return new Position(
+				this.horizontal + by.forward,
+				this.depth + this.aim * by.forward,
+				this.aim,
+			);
+		} else {
+			return new Position(
+				this.horizontal,
+				this.depth,
+				this.aim + by.down - by.up,
+			);
+		}
+	}
+
+	result1(): number {
+		return this.horizontal * this.depth;
+	}
+}
+
+const input: string[] = readFileSync('src/input.txt', 'utf8').split('\n');
+
+console.log(
+	input
+		.map((str) => new Instruction(str))
+		.reduce((pos: Position, move: Instruction) => {
+			const newPosition = pos.move1(move);
+			return newPosition;
+		}, new Position())
+		.result1(),
+);
+
+
+console.log(
+	input
+		.map((str) => new Instruction(str))
+		.reduce((pos: Position, move: Instruction) => {
+			const newPosition = pos.move2(move);
+			return newPosition;
+		}, new Position())
+		.result1(),
+);
